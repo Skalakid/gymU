81b3aa0613673fc34ac7a665ccdfd348d70b976a
Michał Skałka <michal.skalka@swmansion.com>
2024-05-15 02:24:20 +0200

    Configure tab navigation

---
+++ client/components/Icon.tsx
@@ -0,0 +1,34 @@
+// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+import Icons from "@/constants/Icons";
+import { Image, ImageContentFit } from "expo-image";
+import {
+  ImageSourcePropType,
+  type ImageStyle,
+  type StyleProp,
+  type ViewStyle,
+} from "react-native";
+
+type IconProps = {
+  icon: (typeof Icons)[keyof typeof Icons];
+  color: string;
+  size: number;
+  style?: StyleProp<ViewStyle & ImageStyle>;
+  contentFit?: ImageContentFit;
+};
+
+export function Icon({
+  icon,
+  color = "blue",
+  size = 16,
+  style,
+  contentFit = "contain",
+}: IconProps) {
+  return (
+    <Image
+      source={icon as ImageSourcePropType}
+      style={[{ width: size, aspectRatio: 1 }, style]}
+      contentFit={contentFit}
+      tintColor={color}
+    />
+  );
+}
