b54116d0ffcabf8a7e8b31bcecc22763ab4d7063
Michał Skałka <michal.skalka@swmansion.com>
2024-05-27 00:08:28 +0200

    [feature] add sign up page

--- client/app/(auth)/sign-up.tsx
+++ client/app/(auth)/sign-up.tsx
@@ -1,12 +1,20 @@
+import TextLink from "@/components/TextLink";
 import { ThemedText } from "@/components/ThemedText";
+import SecondaryButton from "@/components/button/SecondaryButton";
+import TextInput from "@/components/input/TextInput";
 import PageWithGoBackHeader from "@/components/page/PageWithGoBackHeader";
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 const SignUpPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
   return (
-    <PageWithGoBackHeader title="Sign up">
+    <PageWithGoBackHeader style={styles.container}>
       <View style={styles.headingText}>
-        <ThemedText size="h4" weight="semiBOod">
+        <ThemedText size="h4" weight="semiBold">
           Nice to meet you 🙌
         </ThemedText>
         <ThemedText size="l" weight="medium" textType="description">
@@ -14,6 +22,42 @@ const SignUpPage = () => {
           track your progress and much more!
         </ThemedText>
       </View>
+
+      <TextInput
+        label="Email"
+        placeholder="Enter your email..."
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+      <TextInput
+        label="Password"
+        placeholder="Enter your password..."
+        type="password"
+        onChangeText={(password) => setPassword(password)}
+        value={password}
+      />
+      <TextInput
+        label="Repeat password"
+        placeholder="Repeat your password..."
+        type="password"
+        onChangeText={(password) => setRepeatPassword(password)}
+        value={repeatPassword}
+      />
+
+      <View style={styles.button}>
+        <SecondaryButton
+          value="Register"
+          onPress={() => console.log("Login")}
+        />
+      </View>
+
+      <TextLink
+        style={styles.alreadyHaveAccountLink}
+        onPress={() => console.log("Already have an account?")}
+        size="m"
+      >
+        Don’t have an account? Register
+      </TextLink>
     </PageWithGoBackHeader>
   );
 };
@@ -21,7 +65,22 @@ const SignUpPage = () => {
 export default SignUpPage;
 
 const styles = StyleSheet.create({
+  container: {
+    gap: 10,
+  },
   headingText: {
     gap: 5,
+    marginBottom: 10,
+  },
+  link: {
+    textAlign: "right",
+  },
+  button: {
+    marginTop: 10,
+    marginHorizontal: 40,
+  },
+  alreadyHaveAccountLink: {
+    textAlign: "center",
+    marginTop: 10,
   },
 });
