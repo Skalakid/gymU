d3509beee269c4314e564f8c29a1e49c91784d68
Michał Skałka <39538890+Skalakid@users.noreply.github.com>
2024-05-27 20:29:39 +0200

    [GU-21 GU-26] Configure basic RN App with navigation (#3)
    * [general] setup initial repo directory structure
    
    * [general] add info about repo structure to readme
    
    * [general] update readme
    
    * npx create-expo-app ./client
    
    * Add Poppins font
    
    * Configure tab navigation
    
    * Remove web support
    
    * Add SafeAreaView
    
    * Change tab navigator styles
    
    * Update README and file structure
    
    * Add review changes
    
    * Add review changes
    
    * Migrate all fiels to es6 arrow functions
    
    * Remove unnecessary files
    
    * Remove unnecessary styles
    
    * Fix changing color theme
    
    * Update README
    
    ---------
    
    Co-authored-by: xramzesx <xramzesbusiness@gmail.com>

---
+++ client/app/_layout.tsx
@@ -0,0 +1,97 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import {
+  useFonts,
+  Poppins_100Thin,
+  Poppins_100Thin_Italic,
+  Poppins_200ExtraLight,
+  Poppins_200ExtraLight_Italic,
+  Poppins_300Light,
+  Poppins_300Light_Italic,
+  Poppins_400Regular,
+  Poppins_400Regular_Italic,
+  Poppins_500Medium,
+  Poppins_500Medium_Italic,
+  Poppins_600SemiBold,
+  Poppins_600SemiBold_Italic,
+  Poppins_700Bold,
+  Poppins_700Bold_Italic,
+  Poppins_800ExtraBold,
+  Poppins_800ExtraBold_Italic,
+  Poppins_900Black,
+  Poppins_900Black_Italic,
+} from "@expo-google-fonts/poppins";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+import { Colors } from "@/constants/Colors";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  const [fontsLoaded, fontError] = useFonts({
+    Poppins_100Thin,
+    Poppins_100Thin_Italic,
+    Poppins_200ExtraLight,
+    Poppins_200ExtraLight_Italic,
+    Poppins_300Light,
+    Poppins_300Light_Italic,
+    Poppins_400Regular,
+    Poppins_400Regular_Italic,
+    Poppins_500Medium,
+    Poppins_500Medium_Italic,
+    Poppins_600SemiBold,
+    Poppins_600SemiBold_Italic,
+    Poppins_700Bold,
+    Poppins_700Bold_Italic,
+    Poppins_800ExtraBold,
+    Poppins_800ExtraBold_Italic,
+    Poppins_900Black,
+    Poppins_900Black_Italic,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <SafeAreaView
+        style={[
+          styles.container,
+          {
+            backgroundColor: Colors[colorScheme ?? "light"].background,
+          },
+        ]}
+      >
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </SafeAreaView>
+    </ThemeProvider>
+  );
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ client/constants/Colors.ts
@@ -0,0 +1,28 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
+
+export const Colors = {
+  light: {
+    primary: "#F06312",
+    text: "#11181C",
+    background: "#fff",
+    tint: tintColorLight,
+    icon: "#A8A8A8",
+    tabIconDefault: "#A8A8A8",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    primary: "#F06312",
+    text: "#ECEDEE",
+    background: "#151718",
+    tint: tintColorDark,
+    icon: "#A8A8A8",
+    tabIconDefault: "#A8A8A8",
+    tabIconSelected: tintColorDark,
+  },
+};
