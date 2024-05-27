cf807aa2c90916149ca61e9b7ff1bb7413d56e8b
Michał Bert <michal.bert@swmansion.com>
2024-05-27 20:51:03 +0200

    test

---
+++ services/api/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { Client, Pool } from "pg";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.SERVER_PORT || 3000;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT as unknown as number,
+  database: process.env.DB_SCHEMA,
+});
+
+console.log("eo");
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Basic Node server with TypeScript");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
