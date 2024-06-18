3664b1f2233eba43da0515062532917a567a7c05
Michał Skałka <michal.skalka@swmansion.com>
2024-06-18 23:54:30 +0200

    [fix] remove problems after merge

--- services/api/src/controllers/user.controller.ts
+++ services/api/src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { prisma } from '../config/db.server';
+import { AuthRequest } from '../middlewares/auth.middleware';
 
 type ReturnUser = {
   user_id: number;
@@ -20,7 +21,7 @@ async function checkEmailUniqueness(email: string) {
   }
 }
 
-async function getAllUsers(_req: Request, res: Response) {
+async function getAllUsers(req: AuthRequest, res: Response) {
   try {
     const users: ReturnUser[] = (await prisma.app_user.findMany()).map(
       (user) => ({
@@ -36,5 +37,17 @@ async function getAllUsers(_req: Request, res: Response) {
   }
 }
 
-export { checkEmailUniqueness, getAllUsers };
+async function getCurrentUser(req: AuthRequest, res: Response) {
+  try {
+    const user = req.user as ReturnUser;
+    if (!user) {
+      throw new Error('User not found');
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    return res.send(500).send('Internal server error');
+  }
+}
+
+export { checkEmailUniqueness, getAllUsers, getCurrentUser };
 export type { ReturnUser };
