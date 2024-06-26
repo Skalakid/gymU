bb8490576c46c17e9f6785929882db58f42bbc5f
Michał Skałka <michal.skalka@swmansion.com>
2024-06-26 21:53:21 +0200

    [fix] fix swagger options

--- services/api/src/config/swagger.ts
+++ services/api/src/config/swagger.ts
@@ -2,23 +2,23 @@ import { Express } from 'express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import { setup, serve } from 'swagger-ui-express';
 
-function initSwagger(app: Express) {
-  const options = {
-    swaggerDefinition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'GymU API',
-        version: '0.0.1',
-      },
-      servers: [
-        {
-          url: 'http://localhost:3000',
-        },
-      ],
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'GymU API',
+      version: '0.0.1',
     },
-    apis: ['src/routes//*.ts'],
-  };
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['src/routes//*.ts'],
+};
 
+function initSwagger(app: Express) {
   const specs = swaggerJsdoc(options);
   app.use('/docs', serve, setup(specs, { explorer: true }));
 }
