8312b81b02e693f11e7134bda911df6ca226d6c7
Michał Skałka <michal.skalka@swmansion.com>
2024-06-28 00:43:00 +0200

    [feature] add exercise items

--- client/app/(tabs)/workouts/[id]/exercises.tsx
+++ client/app/(tabs)/workouts/[id]/exercises.tsx
@@ -1,11 +1,45 @@
-import ThemedText from '@/components/ThemedText';
-
-import { StyleSheet } from 'react-native';
+import fetchApi from '@/api/fetch';
+import ExerciseItem from '@/components/exercises/ExerciseItem';
+import { useLocalSearchParams } from 'expo-router';
+import { useCallback, useEffect, useState } from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
 
 const WorkoutDetailsPage = () => {
-  return <ThemedText>Exercises</ThemedText>;
+  const { id } = useLocalSearchParams();
+  const [workoutDetails, setWorkoutDetails] = useState<Workout | null>(null);
+
+  const getWorkoutDetails = useCallback(async () => {
+    try {
+      const response = await fetchApi(`/workout/${id}`, 'GET');
+      if (!response.ok) {
+        console.error(response.statusText);
+        return;
+      }
+      const workoutDetails = await response.json();
+      setWorkoutDetails(workoutDetails);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    getWorkoutDetails();
+  }, [getWorkoutDetails]);
+
+  return (
+    <FlatList
+      style={styles.exerciseList}
+      data={workoutDetails?.exercises || []}
+      renderItem={({ item }) => <ExerciseItem exercise={item} />}
+      ItemSeparatorComponent={() => <View style={{ height: 15 }} />}
+    />
+  );
 };
 
 export default WorkoutDetailsPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  exerciseList: {
+    gap: 10,
+  },
+});
---
+++ client/components/exercises/ExerciseItem.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import ThemedText from '../ThemedText';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import Icon from '../common/Icon';
+import Icons from '@/constants/Icons';
+import Tile from '../common/Tile';
+import useTheme from '@/hooks/useTheme';
+
+type ExerciseItemProp = {
+  exercise: ExerciseItem;
+};
+
+type ExerciseValue = {
+  series?: number;
+  reps?: number;
+  break?: number;
+  time?: number;
+  weight?: number;
+};
+
+const ExerciseItem = ({ exercise }: ExerciseItemProp) => {
+  const theme = useTheme();
+
+  const value: ExerciseValue | null = exercise.value
+    ? JSON.parse(exercise.value)
+    : null;
+
+  return (
+    <TouchableOpacity>
+      <Tile style={styles.container}>
+        <ThemedText size="m" weight="medium">
+          {exercise.exercise_name}
+        </ThemedText>
+        <View style={styles.info}>
+          {(value?.series || value?.reps) && (
+            <View style={styles.row}>
+              <Icon icon={Icons.repeat} size={14} color={theme.text} />
+              <ThemedText size="s">
+                {value.series} series • {value.reps} reps
+              </ThemedText>
+            </View>
+          )}
+
+          {value?.weight && (
+            <View style={styles.row}>
+              <Icon icon={Icons.weight} size={14} color={theme.text} />
+              <ThemedText size="s">{value.weight} kg</ThemedText>
+            </View>
+          )}
+
+          {value?.time && (
+            <View style={styles.row}>
+              <Icon icon={Icons.time} size={14} color={theme.text} />
+              <ThemedText size="s">{value?.time || '~'} seconds</ThemedText>
+            </View>
+          )}
+        </View>
+      </Tile>
+    </TouchableOpacity>
+  );
+};
+export default ExerciseItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    borderRadius: 15,
+    height: 100,
+    justifyContent: 'space-between',
+  },
+  content: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  info: {
+    gap: 3,
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    gap: 5,
+  },
+  tagList: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+  },
+});
