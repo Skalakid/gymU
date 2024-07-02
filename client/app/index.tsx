e27521beb61cd326a159beb4e04df89a19cfd6c5
xramzesx <xramzesbusiness@gmail.com>
2024-07-02 18:01:36 +0200

    [client][ci\cd] trigger eslint error

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -2,6 +2,8 @@ import ThemedView from '@/components/ThemedView';
 import { Colors } from '@/constants/Colors';
 import { ActivityIndicator, StyleSheet } from 'react-native';
 
+import ThemedText from '@/components/ThemedText';
+
 const StartPage = () => {
   return (
     <ThemedView style={styles.container}>
