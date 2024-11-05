18a29e982a52e3f48e5b0d51566d1bc25bfb4484
Michał Bert <michal.bert@swmansion.com>
2024-11-05 18:55:58 +0100

    Add chart to body page

--- client/app/(tabs)/statistics/body/index.tsx
+++ client/app/(tabs)/statistics/body/index.tsx
@@ -1,8 +1,10 @@
+import WeightChart from '@/components/charts/WeightChart';
 import Header from '@/components/navigation/Header';
 import ThemedView from '@/components/ThemedView';
 import Icons from '@/constants/Icons';
 import { useRouter } from 'expo-router';
 import { StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native';
 
 const BodyMeasurements = () => {
   const router = useRouter();
@@ -18,6 +20,10 @@ const BodyMeasurements = () => {
         rightIconSize={26}
         rightIconOnPress={() => router.navigate('/statistics/body/add')}
       />
+
+      <ScrollView contentContainerStyle={styles.chartContainers}>
+        <WeightChart timeInterval={12} />
+      </ScrollView>
     </ThemedView>
   );
 };
@@ -26,4 +32,11 @@ export default BodyMeasurements;
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
+  chartContainers: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+
+    gap: 20,
+  },
 });
