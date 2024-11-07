93e470ced846bee6b00971dc290b8b7df5d89496
xramzesx <xramzesbusiness@gmail.com>
2024-11-07 02:09:12 +0100

    [api] add calendar backend support

---
+++ services/api/src/persistance/calendar.db.ts
@@ -0,0 +1,85 @@
+import { TimeUnit } from '@prisma/client';
+import { prisma } from '../config/db.server';
+import { NewCalendarEvent } from '../types/calendar';
+import { add } from 'date-fns';
+
+type PluralTimeUnit = 'days' | 'weeks' | 'months';
+
+const timeUnitMapper = new Map<TimeUnit, PluralTimeUnit>([
+  ['day', 'days'],
+  ['week', 'weeks'],
+  ['month', 'months'],
+]);
+
+const getNextDate = (datetime: Date, frequency: number, unit: TimeUnit) => {
+  const mappedUnit: PluralTimeUnit = timeUnitMapper.get(unit)!;
+  return add(datetime, { [mappedUnit]: frequency });
+};
+
+async function getAllEventsInRange(
+  userId: number,
+  startDate: Date,
+  endDate: Date,
+) {
+  return await prisma.calendar_event.findMany({
+    where: {
+      datetime: {
+        gte: startDate,
+        lte: endDate,
+      },
+      calendar_event_parent: {
+        user_id: userId,
+      },
+    },
+    include: {
+      calendar_event_parent: {
+        include: {
+          workout_template: {
+            include: {
+              workout_level: true,
+              workout_tags: true,
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+async function createEvent(userId: number, calendarEvent: NewCalendarEvent) {
+  return await prisma.$transaction(async (tx) => {
+    const eventParent = await tx.calendar_event_parent.create({
+      data: {
+        user_id: userId,
+        workout_id: calendarEvent.workoutId,
+        repeat_count: calendarEvent.repeatCount,
+        repeat_frequency: calendarEvent.repeatFrequency,
+        repeat_unit: calendarEvent.repeatUnit,
+      },
+    });
+
+    const parentId = eventParent.parent_id;
+    const events = [];
+
+    let currentDate = calendarEvent.datetime;
+
+    for (let i = 0; i <= calendarEvent.repeatCount; i++) {
+      const event = {
+        parent_id: parentId,
+        datetime: currentDate,
+      };
+
+      events.push(event);
+
+      currentDate = getNextDate(
+        currentDate,
+        calendarEvent.repeatFrequency,
+        calendarEvent.repeatUnit,
+      );
+    }
+
+    await tx.calendar_event.createMany({ data: events });
+  });
+}
+
+export { getAllEventsInRange, createEvent };
---
+++ services/api/src/services/calendar.service.ts
@@ -0,0 +1,34 @@
+import * as CalendarDB from '../persistance/calendar.db';
+import { NewCalendarEvent } from '../types/calendar';
+
+async function getAllEventsInRange(
+  userId: number,
+  startDate: Date,
+  endDate: Date,
+) {
+  const events = await CalendarDB.getAllEventsInRange(
+    userId,
+    startDate,
+    endDate,
+  );
+
+  return events.map((item) => {
+    const workout = item.calendar_event_parent.workout_template;
+    return {
+      eventId: item.event_id,
+      parentId: item.parent_id,
+      datetime: item.datetime,
+      workout: {
+        workoutId: workout.workout_id,
+        name: workout.name,
+        level: workout.workout_level,
+        tags: workout.workout_tags,
+      },
+    };
+  });
+}
+
+async function createEvent(userId: number, calendarEvent: NewCalendarEvent) {
+  return await CalendarDB.createEvent(userId, calendarEvent);
+}
+export { getAllEventsInRange, createEvent };
