0d003a3535acc4747ab42fd2d67a2b2c93683dc8
Michał Bert <michal.bert@swmansion.com>
2024-11-16 17:45:57 +0100

    Fix endpoint

--- client/components/charts/MeasurementChart.tsx
+++ client/components/charts/MeasurementChart.tsx
@@ -30,7 +30,7 @@ const MeasurementChart = ({
   useEffect(() => {
     const getData = async () => {
       const rawData = await fetchApi(
-        `/measurement/${auth.user?.userId}/${timeInterval}`,
+        `/measurement/${auth.user?.userId}/${measurement}/${timeInterval}`,
         'GET',
       );
       const data = await rawData.json();
