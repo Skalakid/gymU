419db02bc8969ba365c523d388b25d0b062f80cb
Michał Skałka <michal.skalka@swmansion.com>
2024-11-16 15:28:10 +0100

    Add eslint and prettier

--- services/asset/src/index.ts
+++ services/asset/src/index.ts
@@ -1,14 +1,13 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.SERVER_PORT || 4000;
 
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("gymU asset service");
+app.get('/', (req: Request, res: Response) => {
+  res.send('gymU asset service');
 });
 
 app.listen(port, () => {
