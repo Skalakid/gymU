9fa48f52d87f886f3e78890634bbff7d9732ebae
Michał Skałka <michal.skalka@swmansion.com>
2024-11-17 19:44:38 +0100

    Add asset serving

--- services/asset/src/index.ts
+++ services/asset/src/index.ts
@@ -1,10 +1,21 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.SERVER_PORT || 4000;
+
+app.get('/assets/:filename', (req: Request, res: Response) => {
+  const { filename } = req.params;
+  if (!filename) {
+    res.status(400).send('Invalid request');
+  }
+
+  const filePath = path.join(__dirname, 'assets', filename);
+  res.sendFile(filePath);
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('gymU asset service');
