8b7c00d478e704e1808549bc3c7e3d68352fcbeb
xramzesx <xramzesbusiness@gmail.com>
2024-11-18 17:33:44 +0100

    [api] rename seed file

--- services/api/prisma/seed.ts
+++ services/api/prisma/seed.ts
@@ -2,7 +2,7 @@ import seedUsers from './exampleSeedData/user.prisma';
 import seedExercises from './exampleSeedData/exersice.prisma';
 import seedWorkouts from './exampleSeedData/workout.prisma';
 import seedTags from './exampleSeedData/tag.prisma';
-import seedScrappedData from './scrappedSeedData/exercise.prisma';
+import seedScrappedData from './scrappedSeedData/scrapped.prisma';
 
 async function seed() {
   await seedUsers();
