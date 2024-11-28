6587b832ceaa02b0a78056050e6dfddaa79fb579
xramzesx <xramzesbusiness@gmail.com>
2024-11-28 15:54:59 +0100

    [analyze] add initial BasketAnalysis structure with database and utils setup

--- services/analyze/main.py
+++ services/analyze/main.py
@@ -1,44 +1,20 @@
 from dotenv import load_dotenv
 from src.models.basket import basket
+from src.services.database import PrismaClient
+from src.utils import save_model, load_model
 
-from prisma import Client
 import asyncio
-import os
 
-load_dotenv()
+
+### MAIN ###
 async def main() -> None:
-    client = Client()
-    await client.connect()
+    ## This function main is currently used for testing purposes
+    ## In the future we will use it for different purposes
+    await basket.train_job()
 
-    workouts = await client.workout_template.find_many(
-        include={
-            'exercise_template_item': True
-        }
-    )
 
-    baskets = []
-
-    products = set()
-    for workout in workouts:
-        exercise_ids = []
-        for exercise in workout.exercise_template_item:
-            exercise_ids.append(exercise.exercise_id)
-            products.add(exercise_ids[-1])
-        
-        baskets.append(tuple(exercise_ids))
-    
-    basket_analysis = basket.BasketMarketAnalysis(
-        baskets,
-        list(products),
-        0.0000001
-    )
-
-    print(basket_analysis.get_supports())
-
-    await client.disconnect()
-
-if __name__ == '__main__':
-    print('why this shit doesn\'t work')
-    print(os.getenv("DATABASE_URL"))
-    # asyncio.run(main())
+### SETUP ENV ###
+if __name__ == "__main__":
+    load_dotenv()
+    print("Analyze service started")
     asyncio.get_event_loop().run_until_complete(main())
