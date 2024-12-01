e2cf8a33fd4fbf01b736bbde4da5e6b8aef6e2b7
Michał Skałka <michal.skalka@swmansion.com>
2024-12-01 12:02:11 +0100

    Add user details to other user profiles

--- client/app/(tabs)/home/user/[id].tsx
+++ client/app/(tabs)/home/user/[id].tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, View } from 'react-native';
-import React, { useMemo } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import ThemedText from '@/components/ThemedText';
 import { useAuthContext } from '@/contexts/AuthContext';
 import { useLocalSearchParams, useRouter } from 'expo-router';
@@ -9,16 +9,40 @@ import Tile from '@/components/common/Tile';
 import Icons from '@/constants/Icons';
 import UserProfile from '@/components/userProfile/UserProfile';
 import EditableUserProfile from '@/components/userProfile/EditableUserProfile';
+import fetchApi from '@/api/fetch';
 
 const UserProfilePage = () => {
   const router = useRouter();
   const { user } = useAuthContext();
   const { id } = useLocalSearchParams();
-  const isCurrentUser = useMemo(() => user?.userId === Number(id), [id, user]);
+  const isCurrentUser = user?.userId === Number(id);
+  const [userDetails, setUserDetails] = useState<User | null>(
+    isCurrentUser ? user : null,
+  );
+
+  const getUserDetails = useCallback(async () => {
+    try {
+      const result = await fetchApi(`/user/${id}`, 'GET');
+      if (!result.ok) {
+        throw new Error(result.status + '');
+      }
+      const data: User = await result.json();
+      setUserDetails(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [id]);
 
   const handleRightIconPress = () => {
     router.navigate('/home/settings');
   };
+
+  useEffect(() => {
+    if (isCurrentUser) {
+      return;
+    }
+    getUserDetails();
+  }, [getUserDetails, isCurrentUser]);
 
   return (
     <PageWithGoBackHeader
@@ -31,7 +55,7 @@ const UserProfilePage = () => {
       {isCurrentUser ? (
         <EditableUserProfile onPress={() => {}} size={112} />
       ) : (
-        <UserProfile onPress={() => {}} size={112} />
+        <UserProfile size={112} />
       )}
 
       <Tile style={styles.tile}>
@@ -39,8 +63,10 @@ const UserProfilePage = () => {
           General info
         </ThemedText>
         <View style={styles.userInfoContent}>
-          <LabeledText label="Username" text={user?.username ?? '-'} />
-          <LabeledText label="Email" text={user?.email ?? '-'} />
+          <LabeledText label="Username" text={userDetails?.username ?? '-'} />
+          {userDetails?.email && (
+            <LabeledText label="Email" text={userDetails?.email ?? '-'} />
+          )}
         </View>
       </Tile>
     </PageWithGoBackHeader>
