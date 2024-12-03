161d01a97a8295bae287a33f97c973e5cb62ea95
xramzesx <xramzesbusiness@gmail.com>
2024-12-03 01:33:17 +0100

    [api] add grpc client support

---
+++ services/api/src/services/analyze.service.ts
@@ -0,0 +1,33 @@
+import * as grpc from '@grpc/grpc-js';
+import { UserRequest } from '../proto/UserRequest';
+import { IdsPackageMessage } from '../proto/IdsPackageMessage';
+import gymuPackage from '../proto.package';
+
+const client = new gymuPackage.BasketAnalyzeService(
+  `${process.env.GRPC_HOST}:${process.env.GRPC_PORT}`,
+  grpc.credentials.createInsecure(),
+);
+
+const recommendExercises = async (userId: number, ids: number[]) =>
+  new Promise((resolve, reject) => {
+    const request: UserRequest = {
+      userId: userId,
+      ids: ids,
+    };
+    client.recommendExercises(
+      request,
+      (error: Error, response: IdsPackageMessage) => {
+        console.log('test', response);
+        if (error) {
+          console.error(error);
+          reject(error);
+        }
+
+        resolve(response);
+      },
+    );
+  });
+
+export default {
+  recommendExercises,
+};
